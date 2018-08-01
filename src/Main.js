3d2fcd6f74739036d21e5441f91f5c5bf7de3de2
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-01 13:05:55 +0400

    Made main component

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React, {component} from 'react'
+
+class Main extends component{
+    render(){
+        return(
+            <div className = "Main">
+                Main
+            </div>
+        )
+    }
+}
+
+export default Main

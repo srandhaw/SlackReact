b841a98259e523c9c303df383eeee2cfddef546a
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-04 19:26:23 +0400

    styled messageList

--- src/MessageList.js
+++ src/MessageList.js
@@ -4,7 +4,14 @@ import Message from './Message'
 const MessageList = (props) => {
 
     return(
-        <div className = "MessageList">
+        <div className = "MessageList" style={styles.messageList}>
+       
+      <div style={styles.roomAnnouncement}>
+        <h3 style={styles.h3}>
+          #general
+        </h3>
+        <p>This is the very beginning of the #general room.</p>
+      </div>
     {
        props.messages.map(msg => (
            <Message message = {msg} key = {msg.id} />
@@ -15,3 +22,19 @@ const MessageList = (props) => {
 }
 
 export default MessageList
+
+const styles = {
+    messageList:{
+        backgroundColor: 'white',
+        flex: 1,
+        paddingBottom: '10rem',
+        overflowY: 'scroll',
+    },
+    roomAnnouncement: {
+        padding: '2rem 1rem',
+      },
+
+      h3: {
+        fontSize: '1.5rem',
+      },
+}

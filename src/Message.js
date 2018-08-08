638e7c419f727c488c020fd2215a62ab3ef40e4d
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-08 15:41:08 +0400

    styled metadata and message

--- src/Message.js
+++ src/Message.js
@@ -5,11 +5,11 @@ import Metadata from './Metadata'
 const Message = (props) =>{
    
         return(
-            <div className = "Message">
+            <div className = "Message" style={styles.message}>
 
  {/* {props.message.user.displayName}: {props.message.content}  */}
  <Avatar user = {props.message.user}/>
- <div className = "details">
+ <div className = "details" style={styles.details}>
  <Metadata message = {props.message}/>
  <div className = "body">
 {props.message.content}
@@ -21,3 +21,15 @@ const Message = (props) =>{
 }
 
 export default Message
+
+const styles = {
+        message: {
+                display: 'flex',
+    marginTop: '1rem',
+    padding: '0 1rem',
+        },
+        details: {
+                flex: 1,
+                paddingLeft: '0.5rem',
+              },
+}
--- src/Metadata.js
+++ src/Metadata.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Metadata = (props) =>{
     return(
-<div className = "Metadata">
-<div>
+<div className = "Metadata" style = {styles.metadata}>
+<div style = {styles.user}>
 {props.message.user.displayName}
     </div>
 
-    <div>
+    <div style = {styles.time}>
         1:10 PM
         </div>
 </div>
@@ -15,4 +15,21 @@ const Metadata = (props) =>{
 }
 
 export default Metadata
+
+const styles = {
+    metadata: {
+display: 'flex',
+alignItems: 'baseline'
+    },
+
+    user: {
+        fontWeight: 'bold',
+        marginRight: '0.5rem',
+      },
+
+      time: {
+        color: '#999',
+        fontSize: '0.8rem',
+      },
+}
 

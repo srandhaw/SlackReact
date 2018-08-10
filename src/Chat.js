f428f05fdc4de456a869fe639c4a5c6b76a9a5cb
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-10 11:40:05 +0400

    added firebase

--- src/Chat.js
+++ src/Chat.js
@@ -10,25 +10,7 @@ class Chat extends Component {
 
     this.state = {
       messages: [
-        {
-            id: 1,
-            user: {
-                uid: '1234234',
-                displayName: 'Sehaj',
-                email: 'srandhaw@purdue.edu',
-            },
-            content: "hey this is sehaj",
-        },
-  
-        {
-            id: 2,
-            user: {
-                uid: '3452123',
-                displayName: 'Shiv',
-                email: 'shivap@purdue.edu',
-            },
-            content: "hi, Im shiv!",
-        },
+        
   
     ]
     }
---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyD339JKEhv4PRBdpGpMKoCmM_SvjWdvIKk",
+    authDomain: "slack2-b8d3e.firebaseapp.com",
+    databaseURL: "https://slack2-b8d3e.firebaseio.com",
+    projectId: "slack2-b8d3e",
+    storageBucket: "",
+    messagingSenderId: "165128951974"
+  }
+
+  const app = firebase.initializeApp(config)

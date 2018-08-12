fdcf1fcdd3b14f11c3bc1fed5445cebb0d1d4cba
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-12 16:23:15 +0400

    passed rooms from main to sidebar

--- src/Main.js
+++ src/Main.js
@@ -10,12 +10,29 @@ class Main extends React.Component {
       name: 's3afternoon',
       description: 'Ask questions and share code',
     },
+
+    rooms: {
+      s3afternoon: {
+        name: 's3afternoon',
+        description: 'Ask questions and share code',
+      },
+
+      general: {
+        name: 'general',
+        description: 'Chat about whatever',
+      },
+
+      random: {
+        name: 'random',
+        description: 'Cat GIFs, etc.',
+      },
+    },
   }
 
   render() {
     return (
       <div className="Main" style = {styles}>
-        <Sidebar user = {this.props.user}  signOut={this.props.signOut}/>
+        <Sidebar user = {this.props.user}  signOut={this.props.signOut} rooms={this.state.rooms}/>
         <Chat user = {this.props.user} room={this.state.room}/>
       </div>
     )
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,7 +12,7 @@ class Sidebar extends Component {
 
          <h1 style={styles.h1}>XTBC 18</h1>
 
-        <RoomList />
+        <RoomList rooms={this.props.rooms}/>
 
 
       </div>

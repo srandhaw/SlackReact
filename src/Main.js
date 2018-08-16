b74c77b96375f1b8592f26be3e86d422986944a2
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-16 16:22:26 +0400

    ...

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 import Sidebar from './Sidebar'
 import Chat from './Chat'
-import base from './base';
+import base from './base'
+import RoomForm from './RoomForm'
 
 class Main extends React.Component {
 
@@ -15,6 +16,15 @@ class Main extends React.Component {
     rooms: {
     
     },
+
+    showRoomForm: false,
+  }
+
+  showRoomForm = () => {
+    this.setState({ showRoomForm: true })
+  }
+   hideRoomForm = () => {
+    this.setState({ showRoomForm: false })
   }
 
   addRoom = (room) =>{
@@ -51,8 +61,14 @@ this.setState({room})
   }
 
   render() {
+
+    if (this.state.showRoomForm) {
+      return <RoomForm addRoom={this.addRoom} />
+    }
+
     return (
       <div className="Main" style = {styles}>
+      
         <Sidebar user = {this.props.user}  signOut={this.props.signOut} rooms={this.state.rooms} setCurrentRoom={this.setCurrentRoom}/>
         <Chat user = {this.props.user} room={this.state.room}/>
       </div>
---
+++ src/RoomForm.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+
+class RoomForm extends React.Component{
+
+    state = {
+        room: {
+            name: '',
+      description: '',
+        },
+    }
+ 
+    handleChange = (ev) =>{
+const room = {...this.state.room}
+room[ev.target.name] = ev.target.value
+this.setState({ room })
+    }
+
+    handleSubmit = ev => {
+        ev.preventDefault()
+        this.props.addRoom(this.state.room)
+      }
+
+    render(){
+        return(
+            <div className = "RoomForm">
+            <main>
+            <h2>Create a room</h2>
+          <form  onSubmit={this.handleSubmit}>
+          <p>
+              <label htmlFor="name">
+                Room Name
+              </label>
+              <input
+                autoFocus
+                required
+                type="text"
+                name="name"
+                value={this.state.room.name}
+                onChange={this.handleChange}
+              />
+            </p>
+
+            <p>
+              <label htmlFor="description">
+                Description
+              </label>
+              <input
+                type="text"
+                name="description"
+                value={this.state.room.description}
+                onChange={this.handleChange}
+              />
+            </p>
+            <div>
+              <button
+                type="submit"
+              >
+                Create Room
+              </button>
+            </div>
+              </form>
+                </main>
+                </div>
+        )
+    }
+}
+
+export default RoomForm
